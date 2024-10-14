ab4b6367e458a345ce1732d403e0f5f54b61e033
am1520x <am1520@ic.ac.uk>
2024-10-14 11:30:40 +0100

    wrote an add person function and updated our data structure to match the example (changed the order of the relationships)

--- group.py
+++ group.py
@@ -2,48 +2,36 @@
 
 # Your code to go here...
 
-my_group =
-"""An example of how to represent a group of acquaintances in Python."""
-
-# Your code to go here...
-
-my_group = 1
-
-
 """
 rows = [{ "name": "", "age": "", "job": "", Relationships: {} }, 
         ]
 """
 rows = [
-        { "name": "Jill", "age": "26", "job": "biologist", "Relationships": {"Friends": ["Zalika"],  "Partner": ["John"]}},
+        { "name": "Jill", "age": "26", "job": "biologist", "Relationships": {"Zalika": "Friend", "John": "Partner"}},
         
-        { "name": "Zalika", "age": "28", "job": "artist", "Relationships": {"Friends": ["Jill"]}, "Landlord": ["Nash"]}, 
+        { "name": "Zalika", "age": "28", "job": "artist", "Relationships": {"Jill": "Friend", "Nash": "Landlord"}}, 
         
-        { "name": "John", "age": "27", "job": "writer", "Relationships": {"Partner": ["Jill"]}, "Cousins": ["Nash"]}, 
+        {"name": "John", "age": "27", "job": "writer", "Relationships": {"Jill": "Partner", "Nash": "Cousin"}}, 
 
-        { "name": "Nash", "age": "34", "job": "chef", "Relationships": {"Cousins": ["John"], "Landlord for": ["Zalika"]}},          
+        { "name": "Nash", "age": "34", "job": "chef", "Relationships": {"John": "Cousin", "Zalika": "Tenant"}},          
         ]
 
-# group_of_friends = []
-
-# for person in rows:
-#     group_of_friends.append(person)
-#     if person["Relationships"] == None:
-
+my_group = []
 def add_person(name, age, job=None, relationships=None):
     person = {"name": name,
         "age": age,
         "job": job if job else None,
         "relationships" : relationships if relationships  else {}
         }
-    return person
-
+    my_group.append(person)
+    print(name, 'added to the group')
 
 for row in rows:
-    for key, entry in enumerate(row):
-        print(entry, row[entry])
+    # for key, entry in enumerate(row):
+    #     print(entry, row[entry])
+    my_group.append(row)
+print(my_group)
 
 
-#add
 
     
